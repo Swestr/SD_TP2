9481baeb42db922e18d32faea81ad4a4e9df941a
Swestr <melanie.gago@bbox.fr>
2018-11-17 17:30:53 +0100

    Suppression doublons

--- JAVA_PROD_CONS/test.java
+++
@@ -1,7 +0,0 @@
-public class test{
-  public static void main(String args[]){
-    System.out.println(new Fonctions().exec("touch file.txt"));
-    System.out.println(new Fonctions().exec("rm file.txt"));
-
-  }
-}
